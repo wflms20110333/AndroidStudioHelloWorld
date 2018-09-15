7971868236563e101454fc34fda3d84ebde7573c
Elizabeth Zou <elizabeth.zou.0@gmail.com>
2018-09-15 11:27:48 -0400

    yay

--- app/src/main/java/com/example/elizabethzou/androidstudiohelloworld/MainActivity.java
+++ app/src/main/java/com/example/elizabethzou/androidstudiohelloworld/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.elizabethzou.androidstudiohelloworld;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((TextView) findViewById(R.id.textView)).setTextColor(getResources().getColor(R.color.colorPrimary));
+            }
+        });
+
+        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.rootView).setBackgroundColor(getResources().getColor(R.color.illuminatiGreen));
+            }
+        });
+
+        findViewById(R.id.button3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newText = ((EditText) findViewById(R.id.editText)).getText().toString() ;
+                if (TextUtils.isEmpty(newText)) {
+                    ((TextView) findViewById(R.id.textView)).setText(R.string.defaultText);
+                } else {
+                    ((TextView) findViewById(R.id.textView)).setText(newText);
+                }
+            }
+        });
+
+        findViewById(R.id.rootView).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((TextView) findViewById(R.id.textView)).setTextColor(getResources().getColor(R.color.black));
+                findViewById(R.id.rootView).setBackgroundColor(getResources().getColor(R.color.colorAccent));
+                ((TextView) findViewById(R.id.textView)).setText(R.string.defaultText);
+            }
+        });
     }
 }
